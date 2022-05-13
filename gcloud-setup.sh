4e0144177dcddbd8a35b231909753656899070f8
Padmasaran <saransathi.97@gmail.com>
2022-05-13 16:10:15 +0530

    Update gcloud-setup to add name var

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -2,6 +2,7 @@
 gcloud config set project evictions-analysis-nyc
 PROJECT_ID=evictions-analysis-nyc
 LOCATION=us-central1
+NAME=eviction_analysis
 
 #enable required APIs for the project
 gcloud services enable bigquery.googleapis.com
@@ -11,22 +12,22 @@ gcloud services enable composer.googleapis.com
 
 #create BQ dataset
 bq --location=$LOCATION mk \
- --dataset eviction_analysis
+ --dataset $NAME
 
 #create GCS buckets
-gsutil mb -l $LOCATION gs://eviction_analysis
+gsutil mb -l $LOCATION gs://$NAME
 
 #create cloud composer instance
-gcloud composer environments create evictions-analysis \
+gcloud composer environments create $NAME \
     --location $LOCATION
 
 #add env variables to cloud composer
 gcloud composer environments update \
-  evictions-analysis \
+  $NAME \
   --location $LOCATION \
-  --update-env-variables=AIRFLOW_VAR_OUTPUT_BUCKET=eviction_analysis
+  --update-env-variables=AIRFLOW_VAR_OUTPUT_BUCKET=$NAME
 
 gcloud composer environments update \
-  evictions-analysis \
+  $NAME \
   --location $LOCATION \
   --update-env-variables=AIRFLOW_VAR_PROJECT=$PROJECT_ID
